b996980e8142c25a0e8be274334b71b51e3e6832
MatKodz <ma@kodz.pro>
2021-12-02 17:46:40 +0100

    final project Tournament

---
+++ bracket/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Tournament from './App';
+import "./style.css";
+//import "./arrow.svg";
+
+ReactDOM.render(<Tournament />, document.getElementById("app"));
